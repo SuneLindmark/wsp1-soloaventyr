18429d886528bb2cdb0098947fdc1dc2b270d2b4
SuneLindmark <sune.lindmark@elev.ga.ntig.se>
2019-10-09 11:19:03 +0200

    Index och Index_layout uppdaterat

--- php/index.php
+++ php/index.php
@@ -1,18 +1,29 @@
 <?php
 
 include 'include/db.php';
+
+$filteredId = 1;
+
+if(isset($_GET['id'])) {
+    // använd get id - url?id=ETT ID
+    //$id = $_GET['id];
+    $filteredId = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+}
+
 $sth = $dbh->prepare('SELECT * FROM story
-                    WHERE id = 1');
+                    WHERE id = :filteredId');
+$sth->bindParam(':filteredId', $filteredId);
 $sth->execute();
 $story = $sth->fetch(PDO::FETCH_ASSOC);
 
 $sth = $dbh->prepare('SELECT * FROM links
                     WHERE story_id = 1');
+$sth->bindParam(':filteredId', $filteredId);
 $sth->execute();
 $links = $sth->fetchAll(PDO::FETCH_ASSOC);
 
 $page_title = "Soloäventyr - Start";
 
-include 'views/index.php';
+include 'views/index_layout.php';
 
 ?>
